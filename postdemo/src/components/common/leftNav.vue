0fdb5f6f118a4392927b77f57e8b301bbb9f10de
stmm666 <you@example.com1084380037@qq.com>
2019-10-17 16:39:10 +0800

    posdemo

---
+++ postdemo/src/components/common/leftNav.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-guarantee"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-home"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-locking"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-child"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-instruction"></i>
+                <div>统计</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.left-nav{
+    color: #fff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    width: 5%;
+}
+.iconfont{
+    font-size: 24px;
+}
+.left-nav ul{
+    padding: 0;
+    margin: 0;
+}
+.left-nav li{
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+    padding: 10px;
+}
+</style>
